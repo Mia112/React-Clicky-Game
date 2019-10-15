c5598dea2b3c1a7046debd313ed2f1d38f15aa42
Mia112 <msheriff112@gmail.com>
2019-10-14 22:34:51 -0400

    Updates

---
+++ precache-manifest.0b44148145ace86e9da5a3a85aa759cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f46a150b376b7f58581b8809d1852e8",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "3b09527531ae273edb2c",
+    "url": "/react-clicky-game/static/css/main.a14504a0.chunk.css"
+  },
+  {
+    "revision": "ec06554a2e757e2b0a1c",
+    "url": "/react-clicky-game/static/js/2.8933f88e.chunk.js"
+  },
+  {
+    "revision": "3b09527531ae273edb2c",
+    "url": "/react-clicky-game/static/js/main.4f582ea2.chunk.js"
+  },
+  {
+    "revision": "4a8b811570584a85356d",
+    "url": "/react-clicky-game/static/js/runtime-main.ee988453.js"
+  }
+]);
