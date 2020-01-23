0649ed9bfeeb514f84bde12c84092c61f24b43b4
Mia112 <msheriff112@gmail.com>
2020-01-22 21:33:07 -0500

    refactor

--- src/App.js
+++ src/App.js
@@ -1,81 +1,91 @@
 import React, { Component } from 'react';
 import './App.css';
-import Card from "./components/Card";
-import Wrapper from "./components/Wrapper";
-import cards from "./cards.json";
+import Card from './components/Card';
+import Wrapper from './components/Wrapper';
+import cards from './cards.json';
 
 class App extends Component {
-  //Setting this.state.cards to the cards json array
-  state = {
-    cards,
-    clickedArray: [],
-    topScore: 0,
-    score: 0,
-    message: "",
-    shakeit: "false"
-  };
-  
-  clickPicture = id => {
-    // Arrange the pictures in a random manner
-    const shuffledArray = this.shuffleArray(cards);
-    this.setState({cards: shuffledArray});
-    // if clicked an image already clicked set this.state.score = 0; empty clickeadArray, end of if block
-    if (this.state.clickedArray.includes(id)) {
-      this.setState({ score: 0, clickedArray: [], message: "Incorrect!! Game Over ☹️ Click an image to play again!", shakeit: "true"});
-    }
-    else {
-      this.setState({
-        clickedArray: this.state.clickedArray.concat([id]),
-        score: this.state.score + 1,
-        message: "Correct!! 🙂",
-        shakeit: "false"
-      });
-    }
-    
-    // set topscore = score if score>topscore.
-    if (this.state.score > this.state.topScore) {
-      this.setState({ topScore: this.state.score });
-    }
-    // shake the wrapper if shakeit is set to true
-  }
-  shuffleArray = (picturesArray) => {
-      for (let i = picturesArray.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [picturesArray[i], picturesArray[j]] = [picturesArray[j], picturesArray[i]];
-      }
-      return picturesArray;
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React Memory Game!!</h1>
-        </header>
-        <h3 className="App-intro">
-        <h2 className = "score"><strong>Your Score: {this.state.score} | Total Score: {this.state.topScore}</strong></h2>
-        <h4 className="message"><strong>{this.state.message}</strong></h4>
-        <strong>Click each emoji once to earn point. 
-        If an emoji is clicked twice, the game will reset and you will lose all of your points!</strong> 
-        </h3>
-        <Wrapper
-        shakeWrapper = {this.state.shakeit}
-        pictures=
-          {this.state.cards.map(picture => (
-            <Card
-              clickPicture={this.clickPicture}
-              id={picture.id}
-              key={picture.id} // to get rid of unique key prop warning
-              image={picture.image}
-            />
-          ))}
-        />
-        <footer className="footer">
-      <div className="container">
-        <span className="text-muted">&copy; Memory Game - React app.</span>
-      </div>
-    </footer> 
-      </div>
-    );
-  }
+	//Setting this.state.cards to the cards json array
+	state = {
+		cards,
+		clickedArray: [],
+		topScore: 0,
+		score: 0,
+		message: '',
+		reset: 'false'
+	};
+
+	clickPicture = id => {
+		const shuffledArray = this.shuffleArray(cards);
+		this.setState({ cards: shuffledArray });
+		if (this.state.clickedArray.includes(id)) {
+			this.setState({
+				topScore: 0,
+				score: 0,
+				clickedArray: [],
+				message: 'Incorrect!! Game Over ☹️ Click an image to play again!',
+				reset: 'true'
+			});
+		} else {
+			this.setState({
+				clickedArray: this.state.clickedArray.concat([id]),
+				score: this.state.score + 1,
+				message: 'Correct!! 🙂',
+				reset: 'false'
+			});
+		}
+		if (this.state.score > this.state.topScore) {
+			this.setState({ topScore: this.state.score });
+		}
+	};
+	shuffleArray = picturesArray => {
+		for (let i = picturesArray.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[picturesArray[i], picturesArray[j]] = [
+				picturesArray[j],
+				picturesArray[i]
+			];
+		}
+		return picturesArray;
+	};
+	render() {
+		return (
+			<div className='App'>
+				<header className='App-header'>
+					<h1 className='App-title'>Welcome to React Memory Game!!</h1>
+				</header>
+				<h3 className='App-intro'>
+					<h2 className='score'>
+						<strong>
+							Currect: {this.state.score} | Total Score: {this.state.topScore}
+						</strong>
+					</h2>
+					<h4 className='message'>
+						<strong>{this.state.message}</strong>
+					</h4>
+					<strong>
+						Click each emoji once to earn point. If an emoji is clicked twice,
+						the game will reset and you will lose all of your points!
+					</strong>
+				</h3>
+				<Wrapper
+					shakeWrapper={this.state.reset}
+					pictures={this.state.cards.map(picture => (
+						<Card
+							clickPicture={this.clickPicture}
+							id={picture.id}
+							key={picture.id}
+							image={picture.image}
+						/>
+					))}
+				/>
+				<footer className='footer'>
+					<div className='container'>
+						<span className='text-muted'>&copy; Memory Game - React app.</span>
+					</div>
+				</footer>
+			</div>
+		);
+	}
 }
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,17 @@
-import React from "react";
-import "./CardStyle.css";
+import React from 'react';
+import './CardStyle.css';
 
 const Card = props => (
-  <div className="card">
-    <div className="img-container">
-      <img className="img-thumbnail img-responsive" alt={props.name} src={props.image} onClick={() => props.clickPicture(props.id)}/>
-    </div>
-  </div>
+	<div className='card'>
+		<div className='img-container'>
+			<img
+				className='img-thumbnail img-responsive'
+				alt={props.name}
+				src={props.image}
+				onClick={() => props.clickPicture(props.id)}
+			/>
+		</div>
+	</div>
 );
-
 
 export default Card;
